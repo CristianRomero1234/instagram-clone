6c30a000af3c06a718b7730946e62e604d872198
Cristian Romero <c.romero.lozano1494@gmail.com>
2023-01-14 15:55:26 +0100

    WIP

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
+
+
 var postArea = document.getElementById("post-area");
 
-// postArea.addEventListener("click", create);
-function create(){
-  for(var i = 0; i< 35; i++){
+// // postArea.addEventListener("click", create);
+function create() {
+  for (var i = 0; i < 35; i++) {
     var post = document.createElement('div');
     post.classList.add("post");
+
     var visuals = document.createElement('div');
     visuals.classList.add("visuals");
+
+    var visualsUserInfo = document.createElement('div');
+    visualsUserInfo.classList.add("visuals-userInfo");
+
+    var userInfoStories = document.createElement('div');
+    userInfoStories.id = "userInfo-stories";
+
+    var userInfoHandle = document.createElement('div');
+    userInfoHandle.id = "userInfo-handle";
+
     var captionArea = document.createElement('div');
     captionArea.classList.add("captionArea");
+
+    visualsUserInfo.appendChild(userInfoStories);
+    visualsUserInfo.appendChild(userInfoHandle);
+    visuals.appendChild(visualsUserInfo);
     post.appendChild(visuals);
     post.appendChild(captionArea);
     postArea.appendChild(post);
   }
-    // console.log(post);
+  console.log(post);
 }
 create();
