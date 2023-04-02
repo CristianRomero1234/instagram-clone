364ecd397f1a280e0b72139cd7f1d7469687fef0
Cristian Romero <c.romero.lozano1494@gmail.com>
2023-04-02 14:22:59 +0200

    test

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 var postArea = document.getElementById("post-area");
 
-function createVisualsArea() {
+function createVisualsArea(...args) {
   let visualsUserInfo = elementCreator(3, 0, "visuals-userInfo", [createDivWithId("userInfo-stories"), createDivWithId("userInfo-handle")]);
-  let visualsImageContainer = elementCreator(3, 0, "visuals-image", elementCreator(4, 'img'))
-
   let visualsBottomNav = populateVisualsAreaBottomNav(elementCreator(0, 1, "visuals-bottomNav"));
-  let visualsArea = elementCreator(3, 0, "visuals", [visualsUserInfo, visualsImageContainer, visualsBottomNav]);//create element with class visuals and add children
-  return visualsArea;
+  if (args[0] == undefined) {
+    let visualsImageContainer = elementCreator(3, 0, "visuals-image", elementCreator(4, 'img'));
+    let visualsArea = elementCreator(3, 0, "visuals", [visualsUserInfo, visualsImageContainer, visualsBottomNav]);//create element with class visuals and add children
+    return visualsArea;
+  } else {
+    let visualsUserInfo = elementCreator(3, 0, "visuals-userInfo", [createDivWithId("userInfo-stories"), createDivWithId("userInfo-handle")]);
+    let visualsImageContainer = elementCreator(3, 0, "visuals-image", elementCreator(7, args[0]));
+    
+    let visualsArea = elementCreator(3, 0, "visuals", [visualsUserInfo, visualsImageContainer, visualsBottomNav]);//create element with class visuals and add children
+    return visualsArea;
+  }
 }
 function populateVisualsAreaBottomNav(visualsBottomNav) {
   let likeIcon = elementCreator(1, 1);
@@ -15,12 +22,21 @@ function populateVisualsAreaBottomNav(visualsBottomNav) {
   addChildren(visualsBottomNav, [likeIcon, commentIcon, sendIcon])
   return visualsBottomNav;
 }
-function createCaptionArea() {
-  let caption = elementCreator(3, 1, "caption", [elementCreator(3, 1, "text", [elementCreator(5, 'p', 1, "commentBody")])])
-  let comments = elementCreator(3, 0, "comments", [caption]);
-  let likesCounterBox = elementCreator(3, 1, "likesCounter", [elementCreator(0, 1, "likesCounter-number"), elementCreator(0, 1, "likesCounter-likes")]);
-  let captionArea = elementCreator(3, 0, "captionArea", [likesCounterBox, comments]);
-  return captionArea;
+function createCaptionArea(...args) {
+  if (args[0] == undefined) {
+    let caption = elementCreator(3, 1, "caption", [elementCreator(3, 1, "text", [elementCreator(5, 'p', 1, "commentBody")])])
+    let comments = elementCreator(3, 0, "comments", [caption]);
+    let likesCounterBox = elementCreator(3, 1, "likesCounter", [elementCreator(0, 1, "likesCounter-number"), elementCreator(0, 1, "likesCounter-likes")]);
+    let captionArea = elementCreator(3, 0, "captionArea", [likesCounterBox, comments]);
+    return captionArea;
+  }else{
+    let caption = elementCreator(3, 1, "caption", [elementCreator(3, 1, "text", [elementCreator(8, args[0])])])
+    let comments = elementCreator(3, 0, "comments", [caption]);
+    let likesCounterBox = elementCreator(3, 1, "likesCounter", [elementCreator(0, 1, "likesCounter-number"), elementCreator(0, 1, "likesCounter-likes")]);
+    let captionArea = elementCreator(3, 0, "captionArea", [likesCounterBox, comments]);
+    return captionArea;
+
+  }
 }
 function createDivWithClass(classString) {//Returns a HTML Object <div> with class of parameter
   var element = document.createElement('div');
@@ -85,38 +101,93 @@ function elementCreator(mode, ...args) {//Dependending on selected mode and para
       return image;
     case 5://fetch post from  jsonplaceholder
       let postBody = document.createElement(args[0]);
-        let postFetched = async () => {
-          await fetch("https://my-json-server.typicode.com/CristianRomero1234/instagramDb/posts/4")
-            .then((response) => response.json())
-            .then((json) => {
-              console.log(json);
-              postBody.innerText = json.content;
-              postBody.style = "text-overflow: ellipsis;"
-              // authorId.innerText = json.userId; 
-              //     // postId.innerText = json.id;
-              //     // postTitle.innerText = json.title;
-            })
-        }
+      let postFetched = async () => {
+        await fetch("https://my-json-server.typicode.com/CristianRomero1234/instagramDb/posts/4")
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json);
+            postBody.innerText = json.content;
+            postBody.style = "text-overflow: ellipsis;"
+            // authorId.innerText = json.userId; 
+            //     // postId.innerText = json.id;
+            //     // postTitle.innerText = json.title;
+          })
+      }
       let returnedValue = postFetched();
       return postBody;
     case 6://return empty image to debug, etc
-    let imageFake = document.createElement('img');
-    let test = ()=>{
-    imageFake.src = "";
-    imageFake.style = "height:300px;width:400px;"
-  }
-  let eee = test();
-  return imageFake;
+      let imageFake = document.createElement('img');
+      let test = () => {
+        imageFake.src = "";
+        imageFake.style = "height:300px;width:400px;"
+      }
+      let eee = test();
+      return imageFake;
+    case 7:
+      // get image url from jsonplaceholder API by Id mode4 extension
+      let imageElement = document.createElement('img');
+      let imageFetchedById = async () => {
+        await fetch(`https://my-json-server.typicode.com/CristianRomero1234/instagramDb/posts/${args[0]}`)
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json);
+            imageElement.src = json.imageUrl;
+            imageElement.style = "height:300px;width:100%; max-width:400px;"
+          })
+      }
+      let urlById = imageFetchedById();
+      
+      return imageElement;
+    case 8:
+      //fetch post from  jsonplaceholder by ID mode5 extension
+      let postBodyelement = document.createElement('p');
+      let postFetchedById = async () => {
+        await fetch(`https://my-json-server.typicode.com/CristianRomero1234/instagramDb/posts/${args[0]}`)
+          .then((response) => response.json())
+          .then((json) => {
+            console.log(json);
+            postBodyelement.innerText = json.content;
+            postBodyelement.style = "text-overflow: ellipsis;"
+            // authorId.innerText = json.userId; 
+            //     // postId.innerText = json.id;
+            //     // postTitle.innerText = json.title;
+          })
+      }
+      let returnedValuebyId = postFetchedById();
+      
+      return postBodyelement;
   }
 }
+
+function createPostStructure2(fakeId) {//Creates a placeholder structure of a post, returns an HTML Object with all the described sections of a post, the object can be manipulated and added API data
+  let post = elementCreator(3, 0, "post", [createVisualsArea(fakeId), createCaptionArea(fakeId)]);
+  return post;
+}
+
+function create2() {
+  for (var i = 0; i < 35; i++) {
+    if (i >0 && i < 9) {
+      let post = createPostStructure2(i);
+      addChildren(postArea, post);
+    } else {
+      var randomId =  Math.floor(Math.random() * 10) > 0 ? Math.floor(Math.random() * 10) : Math.floor(Math.random() * 10);
+      let post = createPostStructure2(randomId);
+      addChildren(postArea, post);
+      
+    }
+
+  }
+}
+
 function createPostStructure() {//Creates a placeholder structure of a post, returns an HTML Object with all the described sections of a post, the object can be manipulated and added API data
   let post = elementCreator(3, 0, "post", [createVisualsArea(), createCaptionArea()]);
   return post;
 }
+
 function create() {
   for (var i = 0; i < 35; i++) {
     let post = createPostStructure();
     addChildren(postArea, post);
   }
 }
-create();
+create2();
